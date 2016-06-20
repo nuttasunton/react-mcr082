c6e22aad4bc19a80b222882bfe4eff59fb24874b
Nuttasuntorn <nuttasunton@mcrconsult.com>
2016-06-20 17:19:33 +0700

    delete list

--- src/components/todo.list.js
+++ src/components/todo.list.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
 
 export default class ToDoList extends Component {
+
+    renderList() {
+        return this.props.todos.map( (todo, index) => {
+            return (
+                <li key={todo.id}> {todo.text}
+                 <button onClick = {this.props.onDelete.bind(null, index)}>del</button> </li>
+            )
+        })
+    }
+
     render() {
+        console.log(this.props.todos )
         return (
             <ul>
-                <li> List </li>
-                <li> List </li>
-                <li> List </li>
+                { this.renderList() }
             </ul>
         )
     }
--- src/main.js
+++ src/main.js
@@ -6,7 +6,31 @@ import { render } from 'react-dom'
 import TodoForm from './components/todo-form.js'
 import TodoList from './components/todo.list.js'
 
+const initialTodos = [
+    {id:1, text: "Go to school", done: false},
+    {id:2, text: "play game", done: false},
+    {id:3, text: "Dota", done: false}
+]
+
 class App extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            todos: initialTodos
+        }
+
+        this.handleDelete = this.handleDelete.bind(this)
+    }
+
+    handleDelete(index) {
+        console.log("delete ", index)
+        let newTodos = this.state.todos
+        newTodos.splice(index, 1)
+
+        this.setState({ todos: newTodos })
+    }
+
     render() {
         return (
             <div>
@@ -14,7 +38,7 @@ class App extends Component {
                 < TodoForm />
 
                 <h1> Todos </h1>
-                < TodoList />
+                < TodoList todos={this.state.todos} onDelete={this.handleDelete} />
             </div>
         )
     }
