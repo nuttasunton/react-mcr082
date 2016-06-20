0c5dec8eb965423631cb2ac2feece6c4d64f7c8a
Nuttasuntorn <nuttasunton@mcrconsult.com>
2016-06-20 15:14:43 +0700

    fisrt commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+var path = require('path')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: './public',
+    }
+}
